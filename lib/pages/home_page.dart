d48182d57eebc760108d22d8f5555522065ae6a6
Dinithi Nanayakkara <166215353+2003dinijay@users.noreply.github.com>
2026-02-10 11:08:32 +0530

    Add HomePage widget with navigation to profile page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page'), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.home, size: 100, color: Colors.blue),
+            SizedBox(height: 20),
+            Text(
+              'Welcome to the Home Page!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to another page or perform an action
+                Navigator.pushNamed(context, '/profile');
+              },
+              child: Text('Go to Profile'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
