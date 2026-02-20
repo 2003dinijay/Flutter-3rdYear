2f9197154e52a3728fb21e28d45403433e7fe901
Dinithi Nanayakkara <166215353+2003dinijay@users.noreply.github.com>
2026-02-20 10:39:44 +0530

    Add Todo model and TodoCard widget for task management

---
+++ todoapp/lib/widgets/todo_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TodoCard extends StatefulWidget {
+  String title;
+  bool isDone;
+  Function onTap;
+  Function delete;
+
+  TodoCard({
+    super.key,
+    required this.title,
+    required this.isDone,
+    required this.onTap,
+    required this.delete,
+  });
+
+  @override
+  State<TodoCard> createState() => _TodoCardState();
+}
+
+class _TodoCardState extends State<TodoCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.black,
+      ),
+
+      child: ListTile(
+        leading: widget.isDone
+            ? Icon(Icons.check_box, color: Colors.green)
+            : Icon(Icons.circle_outlined, color: Colors.white),
+        trailing: IconButton(
+          onPressed: () => widget.delete(),
+          icon: Icon(Icons.delete, color: Colors.red),
+        ),
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
