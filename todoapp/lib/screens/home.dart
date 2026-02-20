7137cd8a2f0268270e9e5788a39c7d83a06a7f41
Dinithi Nanayakkara <166215353+2003dinijay@users.noreply.github.com>
2026-02-20 11:36:02 +0530

    feat: integrate SearchBarInput widget for enhanced task searching functionality

--- todoapp/lib/screens/home.dart
+++ todoapp/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/widgets/todo_card.dart';
+import 'package:todoapp/widgets/search_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,30 +10,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _searchController = TextEditingController();
+
+  void _onSearch(String value) {
+    print("Searching for: $value");
+    // Add your search logic here
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.pinkAccent,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Icon(Icons.menu), Icon(Icons.person)],
+          children: [
+            Icon(Icons.menu, color: Colors.white),
+            Icon(Icons.person, color: Colors.white),
+          ],
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            TodoCard(
-              title: "GYM",
-              isDone: false,
-              onTap: () {
-                print("Tapped on GYM");
-              },
-              delete: () {
-                print("Deleted");
-              },
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SearchBarInput(
+                textEditingController: _searchController,
+                searchData: _onSearch,
+              ),
+              const SizedBox(height: 20),
+              TodoCard(
+                title: "GYM",
+                isDone: false,
+                onTap: () {
+                  print("Tapped on GYM");
+                },
+                delete: () {
+                  print("Deleted");
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- todoapp/lib/widgets/search_bar.dart
+++ todoapp/lib/widgets/search_bar.dart
@@ -18,6 +18,21 @@ class SearchBarInput extends StatefulWidget {
 class _SearchBarInputState extends State<SearchBarInput> {
   @override
   Widget build(BuildContext context) {
-    return TextField();
+    return TextField(
+      controller: widget.textEditingController,
+      decoration: InputDecoration(
+        hintText: "Search",
+        prefixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide.none,
+        ),
+        fillColor: Colors.white,
+        filled: true,
+      ),
+      onChanged: (value) {
+        widget.searchData(value);
+      },
+    );
   }
 }
