664bd7a258f6bbe74a58e4dc3251e7e4cfcb5a5e
Dinithi Nanayakkara <166215353+2003dinijay@users.noreply.github.com>
2026-02-21 23:50:17 +0530

    refactor: enhance todo management with dynamic list and add functionality

--- todoapp/lib/screens/home.dart
+++ todoapp/lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/widgets/todo_card.dart';
 import 'package:todoapp/widgets/search_bar.dart';
+import 'package:todoapp/models/todo.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,22 +12,73 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final TextEditingController _searchController = TextEditingController();
-  bool _isGymDone = false;
+  final TextEditingController _todoController = TextEditingController();
+  List<Todo> _todoList = [Todo(id: '1', title: 'GYM')];
 
   void _onSearch(String value) {
     print("Searching for: $value");
     // Add your search logic here
   }
 
-  void _toggleGymTask() {
+  void _toggleTodo(String id) {
     setState(() {
-      _isGymDone = !_isGymDone;
+      final todo = _todoList.firstWhere((todo) => todo.id == id);
+      todo.toggleDone();
     });
+  }
+
+  void _deleteTodo(String id) {
+    setState(() {
+      _todoList.removeWhere((todo) => todo.id == id);
+    });
+  }
+
+  void _addTodo() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add New Todo'),
+          content: TextField(
+            controller: _todoController,
+            decoration: const InputDecoration(hintText: 'Enter todo title'),
+            autofocus: true,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _todoController.clear();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (_todoController.text.isNotEmpty) {
+                  setState(() {
+                    _todoList.add(
+                      Todo(
+                        id: DateTime.now().toString(),
+                        title: _todoController.text,
+                      ),
+                    );
+                  });
+                  _todoController.clear();
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   void dispose() {
     _searchController.dispose();
+    _todoController.dispose();
     super.dispose();
   }
 
@@ -53,17 +105,31 @@ class _HomeState extends State<Home> {
                 searchData: _onSearch,
               ),
               const SizedBox(height: 20),
-              TodoCard(
-                title: "GYM",
-                isDone: _isGymDone,
-                onTap: _toggleGymTask,
-                delete: () {
-                  print("Deleted");
-                },
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _todoList.length,
+                  itemBuilder: (context, index) {
+                    final todo = _todoList[index];
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: TodoCard(
+                        title: todo.title,
+                        isDone: todo.isDone,
+                        onTap: () => _toggleTodo(todo.id),
+                        delete: () => _deleteTodo(todo.id),
+                      ),
+                    );
+                  },
+                ),
               ),
             ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        backgroundColor: Colors.pinkAccent,
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
